ab3cab958b54f54e2989a34367ffc8dc5be7cab5
Yunus Üstün <yunusustun17@gmail.com>
2022-04-11 02:17:36 +0300

    added sign up form to UserSignupPage component

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -3,7 +3,28 @@ import { Component } from "react";
 class UserSignupPage extends Component {
     render() {
         return (
-            <h1>Sign Up</h1>
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>Username</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>Display Name</label>
+                    <input></input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password"></input>
+                </div>
+                <div>
+                    <label>Password Repeat</label>
+                    <input type="password"></input>
+                </div>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+            </form>
         );
     }
 }
