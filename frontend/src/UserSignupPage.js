0946ec7323bfc895e5bb5b765ff8253490579f87
Yunus Üstün <yunusustun17@gmail.com>
2022-04-11 02:42:29 +0300

    added setState to UserSignupPage component

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -1,28 +1,53 @@
 import { Component } from "react";
 
 class UserSignupPage extends Component {
+
+    state = {
+        username: null,
+        displayName: null,
+        password: null,
+        passwordRepeat: null,
+        agreedClicked: false
+    }
+
+    onChangeAgree = event => {
+        this.setState({
+            agreedClicked: event.target.checked
+        })
+    }
+
+    onChange = event => {
+        const { name, value } = event.target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         return (
             <form>
                 <h1>Sign Up</h1>
                 <div>
                     <label>Username</label>
-                    <input></input>
+                    <input name="username" onChange={this.onChange}></input>
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input></input>
+                    <input name="displayName" onChange={this.onChange}></input>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password"></input>
+                    <input name="password" type="password" onChange={this.onChange}></input>
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password"></input>
+                    <input name="passwordRepeat" type="password" onChange={this.onChange}></input>
                 </div>
                 <div>
-                    <button>Sign Up</button>
+                    <input name="agreedClicked" type="checkbox" onChange={this.onChangeAgree}></input> Agreed
+                </div>
+                <div>
+                    <button disabled={!this.state.agreedClicked}>Sign Up</button>
                 </div>
             </form>
         );
