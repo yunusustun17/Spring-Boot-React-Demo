d917f0d70c56d53b95afe54bc0aa44238c72b366
Yunus Üstün <yunusustun17@gmail.com>
2022-04-11 02:12:55 +0300

    UserSignupPage initial

---
+++ frontend/src/UserSignupPage.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class UserSignupPage extends Component {
+    render() {
+        return (
+            <h1>Sign Up</h1>
+        );
+    }
+}
+
+export default UserSignupPage;
