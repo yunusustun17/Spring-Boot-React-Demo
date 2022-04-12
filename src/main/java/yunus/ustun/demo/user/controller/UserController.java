84c2348c41ad72a19d0e12f778dda86b83da82ab
Yunus Üstün <yunusustun17@gmail.com>
2022-04-12 09:31:02 +0300

    username validation in backend

--- src/main/java/yunus/ustun/demo/user/controller/UserController.java
+++ src/main/java/yunus/ustun/demo/user/controller/UserController.java
@@ -1,12 +1,18 @@
 package yunus.ustun.demo.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import yunus.ustun.demo.common.ApiError;
 import yunus.ustun.demo.common.GenericResponse;
 import yunus.ustun.demo.user.model.User;
 import yunus.ustun.demo.user.service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -15,9 +21,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public GenericResponse createUser(@RequestBody User user) {
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        String username = user.getUsername();
+        if (username == null || username.isEmpty()) {
+            ApiError apiError = new ApiError(400, "Validation Error", "/api/1.0/users");
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "Username is required");
+            apiError.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+        }
+
         userService.save(user);
-        return new GenericResponse("User created successfully");
+        return ResponseEntity.ok(new GenericResponse("User created successfully"));
     }
 
 }
