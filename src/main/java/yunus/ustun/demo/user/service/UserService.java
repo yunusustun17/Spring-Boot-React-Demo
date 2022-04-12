151ba703d350527dea30e8a1e95744ee5ec75ac8
Yunus Üstün <yunusustun17@gmail.com>
2022-04-12 08:10:55 +0300

    encoding password in database

--- src/main/java/yunus/ustun/demo/user/service/UserService.java
+++ src/main/java/yunus/ustun/demo/user/service/UserService.java
@@ -1,6 +1,8 @@
 package yunus.ustun.demo.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import yunus.ustun.demo.user.model.User;
 import yunus.ustun.demo.user.repository.UserRepository;
@@ -9,8 +11,11 @@ import yunus.ustun.demo.user.repository.UserRepository;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    // TODO: 12.04.2022 initialize password encoder with spring boot
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public void save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
