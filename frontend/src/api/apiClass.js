01baed9358a40cabb3dcc27a3b6c04d4f1088fc3
Yunus Üstün <yunusustun17@gmail.com>
2022-04-13 11:01:20 +0300

    login page initial

--- frontend/src/api/apiClass.js
+++ frontend/src/api/apiClass.js
@@ -4,6 +4,10 @@ export const signup = (body) => {
     return axios.post("/api/1.0/users", body);
 }
 
+export const login = (body) => {
+    return axios.post("/api/1.0/users/login", body);
+}
+
 export const changeLanguage = language => {
     axios.defaults.headers['accept-language'] = language;
 }
